807cdd4790a902c61c1940acd4d754ceede9110b
kallaballa <amir@viel-zu.org>
2020-05-03 21:04:43 +0200

    switched to using a library for scrollsnapping
    Former-commit-id: b1f08029efeadc7d4c2fb343eeff656fe11a4c08

--- themes/oliver/assets/js/scrollsnap.temp.js
+++ themes/oliver/assets/js/scrollsnap.temp.js
@@ -1,7 +1,35 @@
 
+var initialized  = false;
+$.fn.isInViewport = function() {
+var elementTop = $(this).offset().top;
+var elementBottom = elementTop + $(this).outerHeight();
+var viewportTop = $(window).scrollTop();
+var viewportBottom = viewportTop + $(window).height();
+return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+window.onscroll = function() { 
+    if(!initialized) {
         $(function() {
-          $.scrollify({
+            $.scrollify({
             section : "figure img",
-//            setHeights: true
-          });
+            updateHash: false,
+            });
         });
+        initialized = true;
+    }
+    
+    var noneVisible = true;
+    $('p').each(function( index ) {
+        noneVisible = noneVisible && !$(this).isInViewport();
+    });
+
+    if(noneVisible) {
+        if($.scrollify.isDisabled())
+            $.scrollify.enable();
+    } else {
+        if(!$.scrollify.isDisabled())
+            $.scrollify.disable();
+    }
+
+}
