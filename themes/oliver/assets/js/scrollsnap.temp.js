11a3805cd55e1a8c26335c8dec6eeae3b07a64d3
kallaballa <amir@viel-zu.org>
2020-05-03 16:17:57 +0200

    switched to using a library for scrollsnapping
    Former-commit-id: 8f0931e33cd6c971e11e63b01a89a2d6d5202be7

--- themes/oliver/assets/js/scrollsnap.temp.js
+++ themes/oliver/assets/js/scrollsnap.temp.js
@@ -1,82 +1,8 @@
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
-    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
 
-    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
-    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-
-    return (vertInView && horInView);
-}
-
-window.onscroll = function() {
-    
-    var galleryImages = document.querySelectorAll("figure img");
-    var viewportImages =[];
-    for (var i = 0; i < galleryImages.length; i++) {
-//        galleryImages[i].style.borderWidth = "0px";
-//        galleryImages[i].classList.remove("scrollTransition");
-        if(isElementInViewport(galleryImages[i])) {
-            viewportImages.push(galleryImages[i]);
-            galleryImages[i].style.borderWidth = "0px";
-        }
-        else {
-            galleryImages[i].classList.remove("sticky");
-//             galleryImages[i].classList.remove("relative");
-        }
-           
-    }
-    console.log(viewportImages.length);
-    
-    if(viewportImages.length == 1) {
-        viewportImages[0].classList.add("sticky");
-    } else {
-    
-        var lower = null;
-        var maxTop = 0;
-
-        for(var i = 0; i < viewportImages.length; i++) {
-            var top = viewportImages[i].getBoundingClientRect().top;
-
-            if(maxTop < top) {
-                maxTop = top;
-                lower = viewportImages[i];
-            }
-        }
-    
-//     var lower = null;
-//     var minTop = 99999;
-//     
-//     for(var i = 0; i < viewportImages.length; i++) {
-//         var top = viewportImages[i].getBoundingClientRect().top;
-// 
-//         if(minTop > top) {
-//             minTop = top;
-//             lower = viewportImages[i];
-//         }
-//     }
-
-        
-//         if(lower != null) {
-//         lower.style.borderWidth = "5px";
-//         lower.style.borderColor = "red";
-//         lower.style.borderStyle = "solid";
-//         }
-        
-        if(lower !== null && (lower.getBoundingClientRect().top < (window.innerHeight - 320))) {
-                  for (var i = 0; i < viewportImages.length; i++) {
-                    if(viewportImages[i].classList.contains("sticky")) {
-                        viewportImages[i].classList.remove("sticky");
-                        viewportImages[i].classList.add("relative");
-                    }
-              
-                }
-                lower.classList.remove("relative");
-                lower.classList.add("sticky");
-//                 setTimeout(2000, function() { lower.classList.remove("fixed"); lower.classList.add("sticky"); });
-              
-        }
-    }
-};
+        $(function() {
+          $.scrollify({
+            section : "figure img",
+            setHeights: false,
+            offset : -80
+          });
+        });
